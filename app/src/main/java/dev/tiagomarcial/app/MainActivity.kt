b41060fc4e55fa3da2a39c41e90f64b60518638d
Tiago Marcial <tiko127@hotmail.com>
2024-07-31 03:17:02 -0300

    Adjusting the programming logic of the first button

--- app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
@@ -1,10 +1,15 @@
 package dev.tiagomarcial.app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val inputText: EditText = findViewById(R.id.edit_number)
+        val randomNumber: Button = findViewById(R.id.button_random)
+        var result: TextView = findViewById(R.id.text_result)
+        randomNumber.setOnClickListener{
+            val text = inputText.text.toString()
+            val inputNumber = text.toInt()
+            val random = Random.Default
+            var numbers = mutableSetOf<Int>()
+        if (inputText.text.toString().isEmpty() || inputNumber !in 6..15){
+            Toast.makeText(this, "Digite um número entre 6 e 15!", Toast.LENGTH_LONG).show()
+        } else {
+            while (true) {
+                val number = random.nextInt(1,60)
+                numbers.add(number)
+                if (inputNumber == numbers.size){
+                    break
+                }
+            }
+        }
+            result.setText("Resultado: $numbers")
         }
     }
 }
