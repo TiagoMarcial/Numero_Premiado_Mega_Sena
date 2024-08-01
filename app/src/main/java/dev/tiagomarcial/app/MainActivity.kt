e59aeeacbf1c37d62833e5c036613aaef7e1b444
Tiago Marcial <tiko127@hotmail.com>
2024-08-01 02:51:37 -0300

    Added functionality to the first button on the main screen

--- app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
@@ -1,5 +1,7 @@
 package dev.tiagomarcial.app
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,26 +24,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // FAZER FUNÇÃO PRIVADA DO BOTÃO E COLOCAR UM NOVO IF DEPOIS DO VAZIO, POS 1 IF SÓ CONVERTE O NULL EM NUMERO E NÃO APLICA O TOAST
         val inputText: EditText = findViewById(R.id.edit_number)
         val randomNumber: Button = findViewById(R.id.button_random)
         var result: TextView = findViewById(R.id.text_result)
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val lastApostate = prefs.getString("result", "Nenhum registro salvo!")
+        if (lastApostate !=null) {
+            result.text = "Último resultado: $lastApostate"
+        }
         randomNumber.setOnClickListener{
             val text = inputText.text.toString()
-            val inputNumber = text.toInt()
-            val random = Random.Default
-            var numbers = mutableSetOf<Int>()
-        if (inputText.text.toString().isEmpty() || inputNumber !in 6..15){
-            Toast.makeText(this, "Digite um número entre 6 e 15!", Toast.LENGTH_LONG).show()
-        } else {
-            while (true) {
-                val number = random.nextInt(1,60)
-                numbers.add(number)
-                if (inputNumber == numbers.size){
-                    break
-                }
-            }
+            numbersRandom(text, result)
+             //APLICANDO UM RESULTADO VAZIO
         }
-            result.setText("Resultado: $numbers")
+    }
+    private fun numbersRandom(text: String, result: TextView) {
+        if (text.isEmpty()){
+            Toast.makeText(this, "Digite um número entre 6 e 15", Toast.LENGTH_LONG).show()
+        } else {
+            val inputNumber = text.toInt()
+            if(inputNumber !in 6..15) {
+                Toast.makeText(this, "Digite um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            } else {
+                val random = Random.Default
+                var numbers = mutableSetOf<Int>()
+                while (true) {
+                    val number = random.nextInt(1, 60)
+                    numbers.add(number)
+                    if (inputNumber == numbers.size) {
+                        break
+                    }
+                }
+                result.setText("Resultado: ${numbers.joinToString ( " - " )}")
+                val lastResult = "${numbers.joinToString ( " - " )}"
+                val editor = prefs.edit()
+                editor.putString("result", lastResult)
+                editor.commit()
+            }
         }
     }
 }
