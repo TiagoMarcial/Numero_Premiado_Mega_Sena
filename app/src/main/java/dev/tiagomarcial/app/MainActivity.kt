ec9e8dc1bed87590ff6115a636ce3ba139f51e81
Tiago Marcial <tiko127@hotmail.com>
2024-08-03 02:45:29 -0300

    Initial commit for button 4 functionality

--- app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
+++ app/src/main/java/dev/tiagomarcial/app/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate
+import java.time.Month
+import java.util.Calendar
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +31,10 @@ class MainActivity : AppCompatActivity() {
         // É BASICAMENTE A MESMA LÓGICA PARA TODOS OS BOTÕES, SÓ MUDA O ESCOPO PASSÍVEL DE SORTEIO,  DAR UM JEITO DE SÓ FAZER 1 VEZ
         val inputText: EditText = findViewById(R.id.edit_number)
         val randomNumber: Button = findViewById(R.id.button_random)
-        var result: TextView = findViewById(R.id.text_result)
-        var mostLuck: Button = findViewById(R.id.button_mostlucky)
-        var leastLuck: Button = findViewById(R.id.button_leastlucky)
+        val result: TextView = findViewById(R.id.text_result)
+        val mostLuck: Button = findViewById(R.id.button_mostlucky)
+        val leastLuck: Button = findViewById(R.id.button_leastlucky)
+        val mostLuckMonth: Button = findViewById(R.id.button_mostluckmonth)
         prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
         val lastApostate = prefs.getString("result", "Nenhum registro salvo!")
         if (lastApostate !=null) {
@@ -50,6 +54,10 @@ class MainActivity : AppCompatActivity() {
             val text = inputText.text.toString()
             numbersRandom(text, result, 3)
         }
+        mostLuckMonth.setOnClickListener {
+            val text = inputText.text.toString()
+            numbersRandom(text, result, 4)
+        }
     }
     private fun numbersRandom(text: String, result: TextView, idButton: Int) {
         if (text.isEmpty()){
@@ -59,6 +67,7 @@ class MainActivity : AppCompatActivity() {
             if(inputNumber !in 6..15) {
                 Toast.makeText(this, "Digite um número entre 6 e 15", Toast.LENGTH_LONG).show()
             } else {
+                //Botão 1 (Gerar números completamente aleatórios)
                 if (idButton == 1) {
                     val random = Random.Default
                     var numbers = mutableSetOf<Int>()
@@ -74,13 +83,14 @@ class MainActivity : AppCompatActivity() {
                     val editor = prefs.edit()
                     editor.putString("result", lastResult)
                     editor.commit()
+                    //Botão 2 (
                 } else if (idButton == 2) {
                     val quantityDraw = inputNumber * 2
                     val avaliableNumbers = listOf(10,53,5,34,23,37,42,30,33,35,4,32,41,11,27,44,17,38,43,28,46,56,16,54,29,49,36,51,6,24,52,2)
                     val filteredNumbers = avaliableNumbers.take(quantityDraw)
-                    var drawnNumberResult = mutableSetOf<Int>()
+                    val drawnNumberResult = mutableSetOf<Int>()
                     while (drawnNumberResult.size < inputNumber){
-                        var drawnNumber = filteredNumbers.sorted().shuffled().take(inputNumber)
+                        val drawnNumber = filteredNumbers.sorted().shuffled().take(inputNumber)
                         drawnNumberResult.addAll(drawnNumber)
                         if (drawnNumberResult.size == inputNumber){
                             break
@@ -96,9 +106,9 @@ class MainActivity : AppCompatActivity() {
                     val quantityDraw = inputNumber * 2
                     val avaliableNumbers = listOf(59,20,13,15,21,55,26,22,3,48,40,31,9,7,1,60,39,57,19,12,14,47,18,58,50,45,8,25,2,52,24,6)
                     val filteredNumbers = avaliableNumbers.take(quantityDraw)
-                    var drawnNumberResult = mutableSetOf<Int>()
+                    val drawnNumberResult = mutableSetOf<Int>()
                     while (drawnNumberResult.size < inputNumber){
-                        var drawnNumber = filteredNumbers.sorted().shuffled().take(inputNumber)
+                        val drawnNumber = filteredNumbers.sorted().shuffled().take(inputNumber)
                         drawnNumberResult.addAll(drawnNumber)
                         if (drawnNumberResult.size == inputNumber){
                             break
@@ -110,7 +120,19 @@ class MainActivity : AppCompatActivity() {
                     editor.putString("result", lastResult)
                     editor.commit()
                 }
+                else if (idButton == 4) {
+                    val calendar = Calendar.getInstance()
+                    val month = calendar.get(Calendar.MONTH) + 1
+                    val avaliableNumbers = listMonth(month)
+                }
             }
+        }
+    }
+    fun listMonth(month: Int) : List<Int> {
+        return when (month) {
+            1 -> listOf(46,13,21,35,44,30,25,52,41,54,28,20,36,42,24,19,59,33,18,17,56,22,12,48,29,47,16,5,38,26,32)
+            2 -> listOf()
+            else -> emptyList()
         }
     }
 }
